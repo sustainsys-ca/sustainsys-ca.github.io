8984dc005874949458e85c87cab8f0de584fdb35
AdamAlAttrach <adamalattrach@gmail.com>
2024-07-18 20:38:30 -0400

    publishing

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [{
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      }]
-    }
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
   };
   
   export default nextConfig;
