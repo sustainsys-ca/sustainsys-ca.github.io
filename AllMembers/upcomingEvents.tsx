9f7fd70319391ce35177ead405593087f1b31303
Bettina Kemme <35538962+bettinakemme@users.noreply.github.com>
2024-09-23 12:24:21 -0400

    Update upcomingEvents.tsx

--- AllMembers/upcomingEvents.tsx
+++ AllMembers/upcomingEvents.tsx
@@ -1,6 +1,7 @@
 const upcomingEvents = [
     // Example { date: "July 15, 2024", title: "Title here", description: "Description here", imageUrl: "/events/<image_here>" },
     // {} start here or by the example below:
-    { date: "August 3, 2024", title: "TBA", description: "TBA", imageUrl: "/events/<image_here>" },
+    { date: "November 11, 2024", title: "Sustainable Data Systems for Data Science", description: "The third edition of the Data Systems meet Data Science (DSDS) workshop is organized by the CREATE SustainSys: Sutainable Data Systems for Data Science .
+The workshop series brings together the research community that works at the intersection of data/software systems, software engineering, and Data Science/AI/ML, either by building the next generation Data Science platforms or by using  AI/ML techniques to improve systems. ", imageUrl: "https://sites.google.com/view/dsds-23/home" },
 ];
 export default upcomingEvents;
