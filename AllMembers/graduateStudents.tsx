d13aa9d1d77e5aa70b8144b67cc898de24ca78e4
Bettina Kemme <35538962+bettinakemme@users.noreply.github.com>
2024-09-23 09:33:57 -0400

    Update graduateStudents.tsx

--- AllMembers/graduateStudents.tsx
+++ AllMembers/graduateStudents.tsx
@@ -4,8 +4,9 @@ const graduateStudents = {
     members: [
         {
             name: 'Michail Bachras',
+            type: 'PhD Student',
             university: 'University of Toronto',
-            image: '',
+            image: '/trainees/MichailBrachas.jpeg',
             linkedin: 'https://www.linkedin.com/in/michalis-bachras-82446a155'
         },
     ]
