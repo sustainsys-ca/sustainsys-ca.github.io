1ee8a7bb45e2ccf5f6a6863d34f0958350134bd0
Bettina Kemme <35538962+bettinakemme@users.noreply.github.com>
2024-09-23 10:03:11 -0400

    Update graduateStudents.tsx

--- AllMembers/graduateStudents.tsx
+++ AllMembers/graduateStudents.tsx
@@ -5,55 +5,55 @@ const graduateStudents = {
         {
             name: 'Michail Bachras, PhD student',
             university: 'University of Toronto',
-            image: '/trainees/MichailBrachas.jpeg',
+            image: '/students/MichailBrachas.jpeg',
             linkedin: 'https://www.linkedin.com/in/michalis-bachras-82446a155'
         },
          {
             name: 'Anurag Chakraborty, PhD student',
             university: 'University of Waterloo',
-            image: '/trainees/AnuragChakraborty.jpeg',
+            image: '/students/AnuragChakraborty.jpeg',
             linkedin: 'https://www.linkedin.com/in/anu7ag/'
         },
          {
             name: 'Jiaxuan Chen, PhD student',
             university: 'McGill University',
-            image: '/trainees/JiaxuanChen.jpeg',
+            image: '/students/JiaxuanChen.jpeg',
             linkedin: 'https://www.linkedin.com/in/jiaxuan-chen-644741216/'
         },
         {
             name: 'Saeed Fathollahzadeh, PhD student',
             university: 'Concordia University',
-            image: '/trainees/SaeedFathollahzadeh.jpeg',
+            image: '/studens/SaeedFathollahzadeh.jpeg',
             linkedin: 'https://www.linkedin.com/in/saeed-fathollahzadeh-2760816b/'
         },
         {
             name: 'Rahma Nouaji, PhD student',
             university: 'McGill University',
-            image: '/trainees/RahmaNouaji.jpeg',
+            image: '/students/RahmaNouaji.jpeg',
             linkedin: 'https://www.linkedin.com/in/rahma-nouaji/'
         },
         {
             name: 'Reham Omar, PhD student',
             university: 'Concordia University',
-            image: '/trainees/RehamOmar.jpeg',
+            image: '/students/RehamOmar.jpeg',
             linkedin: 'https://www.linkedin.com/in/reham93/'
         },
         {
             name: 'Mohaddeseh Yaghoubpour, PhD student',
             university: 'McGill University',
-            image: '/trainees/MohaddesehYaghoubpour.jpeg',
+            image: '/students/MohaddesehYaghoubpour.jpeg',
             linkedin: ''
         },
         {
             name: 'Yuqiu Zhang, PhD student',
             university: 'University of Toronto',
-            image: '/trainees/YuqiuZhang.jpg',
+            image: '/students/YuqiuZhang.jpg',
             linkedin: 'https://www.linkedin.com/in/yuqiuzhang/'
         },
         {
             name: 'Shiquan Zhang, PhD student',
             university: 'University of Toronto',
-            image: '/trainees/ShiquanZhang.jpg',
+            image: '/students/ShiquanZhang.jpg',
             linkedin: 'www.linkedin.com/in/shiquan-zhang-0535752bb'
         },
     ]
