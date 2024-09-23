99e1115628752ec3f2a2073279058c20e03406d8
Bettina Kemme <35538962+bettinakemme@users.noreply.github.com>
2024-09-23 12:49:31 -0400

    Update upcomingEvents.tsx

--- AllMembers/upcomingEvents.tsx
+++ AllMembers/upcomingEvents.tsx
@@ -3,13 +3,7 @@ const upcomingEvents = [
     // {} start here or by the example below:
     { date: "November 11, 2024", 
       title: "Sustainable Data Systems for Data Science", 
-      description: "The third edition of the Data Systems meet Data Science (DSDS) workshop is organized by the CREATE SustainSys: Sutainable Data Systems for Data Science .
-The workshop series brings together the research community that works at the intersection of data/software systems, software engineering, and Data Science/AI/ML, either by building the next generation Data Science platforms or by using  AI/ML techniques to improve systems. 
-Being now embedded in the CREATE program, the workshop will partially focus on sustainability and energy-efficiency of the systems and platform that support or employ ML.
-Technical presentations from guest speakers in industry and academia will be augmented with poster and demo sessions from students, as well as time and space for discussions.
-
-The workshop is co-organized by Bettina Kemme, Essam Mansour, Hans-Arno Jacobsen, Natalie Enright-Jerger, Oana Balmau, Semih Salihoğlu
-", 
+      description: "The third edition of the Data Systems meet Data Science (DSDS) workshop  udents,  Natalie Enright-Jerger, Oana Balmau",
        imageUrl: "/events/SDSDS2024.jpeg" 
     },
 ];
