af1f0a7bd71857f1b87f112e9d7eb6f25582650a
Bettina Kemme <35538962+bettinakemme@users.noreply.github.com>
2024-09-23 09:58:34 -0400

    Update graduateStudents.tsx

--- AllMembers/graduateStudents.tsx
+++ AllMembers/graduateStudents.tsx
@@ -8,6 +8,54 @@ const graduateStudents = {
             image: '/trainees/MichailBrachas.jpeg',
             linkedin: 'https://www.linkedin.com/in/michalis-bachras-82446a155'
         },
+         {
+            name: 'Anurag Chakraborty, PhD student',
+            university: 'University of Waterloo',
+            image: '/trainees/AnuragChakraborty.jpeg',
+            linkedin: 'https://www.linkedin.com/in/anu7ag/'
+        },
+         {
+            name: 'Jiaxuan Chen, PhD student',
+            university: 'McGill University',
+            image: '/trainees/JiaxuanChen.jpeg',
+            linkedin: 'https://www.linkedin.com/in/jiaxuan-chen-644741216/'
+        },
+        {
+            name: 'Saeed Fathollahzadeh, PhD student',
+            university: 'Concordia University',
+            image: '/trainees/SaeedFathollahzadeh.jpeg',
+            linkedin: 'https://www.linkedin.com/in/saeed-fathollahzadeh-2760816b/'
+        },
+        {
+            name: 'Rahma Nouaji, PhD student',
+            university: 'McGill University',
+            image: '/trainees/RahmaNouaji.jpeg',
+            linkedin: 'https://www.linkedin.com/in/rahma-nouaji/'
+        },
+        {
+            name: 'Reham Omar, PhD student',
+            university: 'Concordia University',
+            image: '/trainees/RehamOmar.jpeg',
+            linkedin: 'https://www.linkedin.com/in/reham93/'
+        },
+        {
+            name: 'Mohaddeseh Yaghoubpour, PhD student',
+            university: 'McGill University',
+            image: '/trainees/MohaddesehYaghoubpour.jpeg',
+            linkedin: ''
+        },
+        {
+            name: 'Yuqiu Zhang, PhD student',
+            university: 'University of Toronto',
+            image: '/trainees/YuqiuZhang.jpg',
+            linkedin: 'https://www.linkedin.com/in/yuqiuzhang/'
+        },
+        {
+            name: 'Shiquan Zhang, PhD student',
+            university: 'University of Toronto',
+            image: '/trainees/ShiquanZhang.jpg',
+            linkedin: 'www.linkedin.com/in/shiquan-zhang-0535752bb'
+        },
     ]
 };
 export default graduateStudents;
