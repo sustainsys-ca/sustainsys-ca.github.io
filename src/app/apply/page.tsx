d9d6cc047f44cb95f43a81f5600dbf55ee99be1a
Bettina Kemme <35538962+bettinakemme@users.noreply.github.com>
2024-11-04 11:15:50 -0500

    Update page.tsx
    changed sentence

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,7 +1,7 @@
 export default function Apply() {
     return (
         <div className="my-32 ml-11">
-            Information on how to apply coming soon!
+            We recommend students interested in this program to apply to one of the partner universities, indicating one of the co-PIs as possible supervisors (see core team).
         </div>
     )
 }
